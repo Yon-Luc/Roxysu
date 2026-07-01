3852648b218bd978909a563610e35035db4dfbcc
Yonluc <lucas@shopand.co>
2026-07-01 11:05:54 +0100

    Add initial project setup with bun, including configuration files and basic React app structure

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
-
+import { staticPlugin } from "@elysiajs/static";
+const app = new Elysia()
+  .use(
+    await staticPlugin({
+      assets: "public",
+      prefix: "/",
+      indexHTML: true,
+      bunFullstack: true,
+    }),
+  )
+  .listen(3000);
+console.log("🦊 Running at http://localhost:3000/public");
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
